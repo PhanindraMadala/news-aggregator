a03662bfc35c07d01ac5edfaebb7d1db1a412fbd
MADALA PHANINDRA <phanindra5015@gmail.com>
2025-09-21 14:51:01 +0530

    Added background image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      backgroundColor: "white",
+      height: "100vh",
+      width: "100vw",
+      padding: "10px"
+    }}>
+      <span style={{ color: "black", fontSize: "14px" }}>news</span>
     </div>
   );
 }
